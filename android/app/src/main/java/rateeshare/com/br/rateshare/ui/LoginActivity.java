b24322b29483e03e5c67abd698ca5b170daf50ae
Robson Langkammer SERVICO <robsonlangkammer@hotmail.com>
2018-11-09 17:53:23 -0200

    melhorias nas cores e novas logos

---
+++ android/app/src/main/java/rateeshare/com/br/rateshare/ui/LoginActivity.java
@@ -0,0 +1,23 @@
+package rateeshare.com.br.rateshare.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import rateeshare.com.br.rateshare.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
