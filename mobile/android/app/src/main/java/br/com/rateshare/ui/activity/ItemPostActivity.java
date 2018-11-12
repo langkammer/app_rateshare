d82a88b8ef99e4b042f56b455884fff6adf9659c
Robson Langkammer SERVICO <robsonlangkammer@hotmail.com>
2018-11-12 17:58:19 -0200

    melhorias na lista itens

---
+++ mobile/android/app/src/main/java/br/com/rateshare/ui/activity/ItemPostActivity.java
@@ -0,0 +1,15 @@
+package br.com.rateshare.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.rateshare.R;
+
+public class ItemPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_post);
+    }
+}
